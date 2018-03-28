76be75bff8396578269e8207ff7ba2abc4ead471
Nadia Omer <nads@admasuniversity.com>
2018-03-29 00:03:51 +0300

    connect and live reload added

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	coffee = require('gulp-coffee'),
 	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass'),
+	connect = require('gulp-connect'),
 	concat= require('gulp-concat');
 var coffeeSourses=['components/coffee/tagline.coffee'];
 var jsSources =[
@@ -10,9 +11,9 @@ var jsSources =[
 		'components/scripts/pixgrid.js',
 		'components/scripts/tagline.js',
 		'components/scripts/template.js'
-	]
-var sassSources = ['components/sass/style.scss']
-
+	];
+var sassSources = ['components/sass/style.scss'];
+var htmlSources =['builds/development/*html']
 	gulp.task('coffee', function(){
 		gulp.src(coffeeSourses)
 			.pipe(coffee({bare : true})
@@ -25,6 +26,7 @@ var sassSources = ['components/sass/style.scss']
 			.pipe(concat('script.js'))
 			.pipe(browserify())
 			.pipe(gulp.dest('builds/development/js'))
+			.pipe(connect.reload())
 
 	})
 
@@ -37,4 +39,32 @@ var sassSources = ['components/sass/style.scss']
 			})
 			.on('error', gutil.log))
 			.pipe(gulp.dest('builds/development/css')) 
+			.pipe(connect.reload())
 	})
+
+	gulp.task('watch', function(){
+		gulp.watch(coffeeSourses,['coffee']);
+		gulp.watch(jsSources,['js']);
+		gulp.watch('components/sass/*.scss',['compass']);
+		gulp.watch(htmlSources,['html']);
+		gulp.watch('builds/development/js/*.json',['json']);
+	})
+
+	gulp.task('connect',function(){
+		connect.server({
+			root: 'builds/development',
+			livereload: true
+		})
+
+	})
+
+	gulp.task('html', function(){
+		gulp.src(htmlSources)
+			.pipe(connect.reload())
+	})
+	gulp.task('json', function(){
+		gulp.src('builds/development/js/*.json')
+			.pipe(connect.reload())
+	})
+
+	gulp.task('default',['html','json', 'coffee','js','compass', 'connect', 'watch'])
