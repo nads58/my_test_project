6797ca5d17f23d1cc8d6f336e819ff4f43241c62
Nadia Omer <nads@admasuniversity.com>
2018-03-28 20:42:39 +0300

    Added browserify ....

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 	gutil=require('gulp-util'),
 	coffee = require('gulp-coffee'),
+	browserify = require('gulp-browserify'),
 	concat= require('gulp-concat');
 var coffeeSourses=['components/coffee/tagline.coffee'];
 var jsSources =[
@@ -20,6 +21,7 @@ var jsSources =[
 	gulp.task('js', function(){
 		gulp.src(jsSources)
 			.pipe(concat('script.js'))
+			.pipe(browserify())
 			.pipe(gulp.dest('builds/development/js'))
 
 	})
