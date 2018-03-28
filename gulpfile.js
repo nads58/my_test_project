7866421f4b7ad0d201a694669f1d3fcae28730ba
Nadia Omer <nads@admasuniversity.com>
2018-03-28 19:48:24 +0300

    Added coffee script

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp'),
-	gutil=require('gulp-util');
+	gutil=require('gulp-util'),
+	coffee = require('gulp-coffee');
+var coffeeSourses=['components/coffee/tagline.coffee'];
 	
 
-	gulp.task('log', function(){
-		gutil.log('Assalamu Alaikum');
-	})
+	gulp.task('coffee', function(){
+		gulp.src(coffeeSourses)
+			.pipe(coffee({bare : true})
+				.on('error', gutil.log))
+			.pipe(gulp.dest('components/scripts'))
+	});
